0ad525c3a3ceac474ca378312ae65f23a9d8e544
--local <--local>
2019-01-03 17:46:29 -0300

    initial commit

---
+++ desktop/src/com/lagecompany/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.lagecompany.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.lagecompany.InfinityGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new InfinityGame(), config);
+	}
+}
